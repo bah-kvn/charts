9b3d7163ed76f9e1b42e12371465ebc42dd49dd9
Kevin Hansen <95251398+bah-kvn@users.noreply.github.com>
2022-12-19 14:42:09 -0600

    New chart version

--- example.sh
+++ example.sh
@@ -2,3 +2,8 @@ helm package /usr/local/git/factory/
 helm repo index . # create or update the index.yaml for repo
 git add .
 git commit -m 'New chart version'
+git push
+
+helm repo add factory 'https://raw.githubusercontent.com/bah-kvn/charts/main/'
+helm repo update
+helm search repo factory
--- factory/examples/pkg.sh
+++
@@ -1,5 +0,0 @@
-
-
-git add . --all ; git commit -a -m" $1 "  ; git push
-
-cd ../charts && ./pkg.sh "$1" && cd -
