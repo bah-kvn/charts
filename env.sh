9a647c155db5d3fdc5df20d1f93c05e08627d980
Kevin Hansen <95251398+bah-kvn@users.noreply.github.com>
2022-12-19 14:00:03 -0600

    test

---
+++ env.sh
@@ -0,0 +1,9 @@
+
+
+export CR_OWNER="bah-kvn"
+export CR_GIT_REPO="charts"
+export CR_PACKAGE_PATH=.deploy
+export CR_TOKEN="$GH_PAT"
+export CR_GIT_BASE_URL="https://api.github.com/"
+export CR_GIT_UPLOAD_URL="https://uploads.github.com/"
+export CR_SKIP_EXISTING=false
