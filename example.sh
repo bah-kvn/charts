bb8a23ab52fbf1c637e7dfcdd60a82a0cfee758e
Kevin Hansen <95251398+bah-kvn@users.noreply.github.com>
2023-01-03 15:44:00 -0600

    New chart version: 0.2.8

--- example.sh
+++ example.sh
@@ -1,7 +1,9 @@
 helm package /usr/local/git/factory/
 helm repo index . # create or update the index.yaml for repo
+export version=$(cat ../factory/Chart.yaml | yq '.version')
+
 git add .
-git commit -m 'New chart version'
+git commit -m "New chart version: $version"
 git push
 
 helm repo add factory 'https://raw.githubusercontent.com/bah-kvn/charts/main/'
